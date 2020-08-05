3784cb7bbcb76cc134f116ca2b99d4c6cf2d8361
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2020-08-05 18:58:30 +1200

    Update wsus.ps1

--- powershell/wsus.ps1
+++ powershell/wsus.ps1
@@ -1,3 +1,17 @@
-# Pre-requisite's     
-# Internet Information Services (IIS)
-# Minimum, .NET Framework 2.0
+# run the EXE you did download to download an ISO media
+# [/Mediapath=***] ⇒ target folder to download
+
+SQL2019-SSEI-Eval.exe /Action=Download /Mediatype=ISO /Language=en-EN /Mediapath=C:\Users\Administrator /Verbose /Quiet 
+
+# language might need to be changed to en or EN only
+
+Mount-DiskImage C:\Users\Administrator\SQLServer2019-x64-ENU.iso
+# Mount the disk image
+
+Get-Volume
+# confirm where drive ISO mounted
+
+cd D:\
+# replace D with the drive letter assoicated with the mounted ISO
+
+ls
